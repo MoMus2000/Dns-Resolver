95bf25c5dc85a86b2cf53d1a1262bb8cdf20b0b7
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2024-03-21 03:48:10 -0400

    Getting final ip, but they don't work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::borrow::Borrow;
 use std::{self, thread};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use dns_parser::{self, Packet, Question, Builder};
+use dns_parser::rdata::ns::Record;
+use dns_parser::{self, Builder, Packet, Question, RData};
 
 struct RootSevers<'a>{
     root_servers: Vec<&'a str>
@@ -10,18 +12,18 @@ impl <'a> RootSevers<'a>{
     pub fn new() -> Self{
         let mut root_servers = Vec::<&'a str>::new();
         root_servers.push("198.41.0.4");
-        root_servers.push("199.9.14.201");
-        root_servers.push("192.33.4.12");
-        root_servers.push("199.7.91.13");
-        root_servers.push("192.203.230.10");
-        root_servers.push("192.5.5.241");
-        root_servers.push("192.112.36.4");
-        root_servers.push("198.97.190.53");
-        root_servers.push("192.36.148.17");
-        root_servers.push("192.58.128.30");
-        root_servers.push("193.0.14.129");
-        root_servers.push("199.7.83.42");
-        root_servers.push("202.12.27.33");
+        // root_servers.push("199.9.14.201");
+        // root_servers.push("192.33.4.12");
+        // root_servers.push("199.7.91.13");
+        // root_servers.push("192.203.230.10");
+        // root_servers.push("192.5.5.241");
+        // root_servers.push("192.112.36.4");
+        // root_servers.push("198.97.190.53");
+        // root_servers.push("192.36.148.17");
+        // root_servers.push("192.58.128.30");
+        // root_servers.push("193.0.14.129");
+        // root_servers.push("199.7.83.42");
+        // root_servers.push("202.12.27.33");
         RootSevers { root_servers }
     }
 }
@@ -67,10 +69,60 @@ fn dns_query(question: &Question, root_servers: Vec<&str>){
 
         // Redirects me to visit the name servers
         let packet = Packet::parse(&buf[..nbytes]).unwrap();
+        
+        println!("Is Authoritative: {:?}", packet.header.authoritative);
+        if !packet.header.authoritative{
+            println!("Answers: {:?}", packet.answers);
+            let nses = packet.nameservers;
+            for ns in nses {
+                match ns.data{
+                    RData::NS(Record(ip)) => {
+                        let name_server_address = ip.to_string();
+                        let inner_sock = socket.try_clone().unwrap();
 
-        println!("Answers: {:?}", packet.answers);
-        println!("NameServer(s) {:?}", packet.nameservers);
-        println!("Opt: {:?}", packet.opt);
+                        let mut message = Builder::new_query(1, true);
+                        message.add_question(question.qname.to_string().as_str(), question.prefer_unicast, question.qtype, question.qclass);
+                        let bytes = message.build().unwrap();
 
-    }
-}
+                        let mut buf = [0; 512];
+                        let _ = inner_sock.connect(name_server_address.to_string()+":53").unwrap();
+                        let _ = socket.send(&bytes).unwrap();
+                        let nbytes = socket.recv(&mut buf).unwrap();
+                        let packet = Packet::parse(&buf[..nbytes]).unwrap();
+
+                        if !packet.header.authoritative{
+                            println!("Answers: {:?}", packet.answers);
+                            let nses = packet.nameservers;
+                            for ns in nses {
+                                match ns.data{
+                                    RData::NS(Record(ip)) => {
+                                        let name_server_address = ip.to_string();
+                                        let inner_sock = socket.try_clone().unwrap();
+
+                                        let mut message = Builder::new_query(1, true);
+                                        message.add_question(question.qname.to_string().as_str(), question.prefer_unicast, question.qtype, question.qclass);
+                                        let bytes = message.build().unwrap();
+
+                                        let mut buf = [0; 512];
+                                        let _ = inner_sock.connect(name_server_address.to_string()+":53").unwrap();
+                                        let _ = socket.send(&bytes).unwrap();
+                                        let nbytes = socket.recv(&mut buf).unwrap();
+                                        let packet = Packet::parse(&buf[..nbytes]).unwrap();
+
+                                        if packet.header.authoritative{
+                                            println!("Final Response: {:?}", packet);
+                                            return
+                                        }
+                                        }
+                                            _ => println!("Ignore")
+                                        }
+                                }
+                            }
+                        }
+                        _ => println!("Ignore")
+                    }
+                }
+                }
+                println!("Opt: {:?}", packet.opt);
+            }
+        }
